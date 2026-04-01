e9bcfed486919a0c9f7693b6523195f01e76ac33
Mostakim <mostakim2005arafat@gmail.com>
2026-04-01 09:07:52 +0600

    tool install

--- Digital_shop/src/App.jsx
+++ Digital_shop/src/App.jsx
@@ -7,7 +7,8 @@ function App() {
 
   return (
     <>
-     <h1>hello</h1>
+     <h1 className='text-4xl text-green-400'>hello</h1>
+     <button className='btn btn-primary'>click</button>
     </>
   )
 }
